ce8d357df2edce6e29e05013e3d9c1cf346ce178
minhtc900 <minhtc900@gmail.com>
2022-03-06 12:09:11 +0700

    updated

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_interface" "test" {
-  name                = "${var.application_type}-${var.resource_type}-nic"
+  name                = "udacity-nic"
   location            = var.location
   resource_group_name = var.resource_group
 
@@ -12,7 +12,7 @@ resource "azurerm_network_interface" "test" {
 }
 
 resource "azurerm_linux_virtual_machine" "test" {
-  name                = "${var.application_type}-${var.resource_type}"
+  name                = var.name
   location            = var.location
   resource_group_name = var.resource_group
   size                = "Standard_B1s"
@@ -20,9 +20,10 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = var.vm_admin_username
-    public_key = file("/home/vsts/work/_temp/udacity_azure.pub")
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCn6wutENkms2AHL2UUQcJlh2+qFBZRhYpSZ+tSRERAyedG3n4AtDDrgiNXdAYsuAa2AGWrvrcoYHDdyYXvxsXmVQu1cKgaKMnSqglD2TcZyPLLFr5aJ0BEegGUU9ddSMFUi/UsQ5nbtVm9To6Ke7b496PDBAp4fnorn8QJXEj9GguToKuiZFEpH1mxlunOcTDgkqB/RR+h63XwxO3hwaRs92L0BsqjzAeFA5tUAvG/nXQJ3gx9/FJ6Ny/cVmvyTj45ol7uFpx4EoR1nKyIa1aJrnsvqoSeYJXQQj/6eeaE0rpPE/0GrnZKPfGD0MF/aH8uzr/Rh5ZkZSJTYFL626ZXFHy3+1MDJ2T/YCn12vlP48RaAPgil3m5h8Pajw/RatedIuTH49OKlAdvNnSSoreI7cw+xWhyuSQWYKOKe/btmCJ2IEhSBV35d4r9vRDLgROo1FBUiUH+mIDkWi83bkOMEDo+OgkTU/d+YU8avdty3AE0xuHsry00FKneySRViak= minhtc900@Minhs-MacBook-Air.local"
   }
   os_disk {
+    name              = "myOsDisk"
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -32,4 +33,7 @@ resource "azurerm_linux_virtual_machine" "test" {
     sku       = "18.04-LTS"
     version   = "latest"
   }
+  computer_name  = "myvm"
+  admin_username = var.admin_username
+  disable_password_authentication = true
 }
