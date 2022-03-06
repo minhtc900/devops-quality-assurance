84d08980e530bb28537c1606b11a745cc2682f18
minhtc900 <minhtc900@gmail.com>
2022-03-06 12:40:53 +0700

    test

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -16,7 +16,8 @@ resource "azurerm_linux_virtual_machine" "test" {
   location            = var.location
   resource_group_name = var.resource_group
   size                = "Standard_B1s"
-  admin_username      = var.vm_admin_username
+  admin_username = var.vm_admin_username
+  disable_password_authentication = true
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = var.vm_admin_username
@@ -32,7 +33,4 @@ resource "azurerm_linux_virtual_machine" "test" {
     sku       = "18.04-LTS"
     version   = "latest"
   }
-  computer_name  = "myvm"
-  admin_username = var.vm_admin_username
-  disable_password_authentication = true
 }
